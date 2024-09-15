fe24bc077d0676077026294732ef303f3da056c8
Kolawole Peter <oluwatayokolawole12@gmail.com>
2024-09-15 23:46:45 +0100

    DD

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -8,3 +8,14 @@ urlpatterns = [
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('profile/', views.profile, name='profile'),  # Make sure you have a profile view
 ]
+
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('posts/', views.PostListView.as_view(), name='post-list'),
+    path('posts/<int:pk>/', views.PostDetailView.as_view(), name='post-detail'),
+    path('posts/new/', views.PostCreateView.as_view(), name='post-create'),
+    path('posts/<int:pk>/edit/', views.PostUpdateView.as_view(), name='post-update'),
+    path('posts/<int:pk>/delete/', views.PostDeleteView.as_view(), name='post-delete'),
+]
