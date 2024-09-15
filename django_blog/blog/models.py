db694cddfcb667589402dd78c5ffe34e3a115c14
Kolawole Peter <oluwatayokolawole12@gmail.com>
2024-09-15 19:38:19 +0100

    GG

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -9,3 +9,21 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+    
+from django.contrib.auth.models import User
+from django.db import models
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(max_length=500, blank=True)
+    profile_picture = models.ImageField(upload_to='profile_pics', blank=True)
+
+# Signal to automatically create a profile when a user is created
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from django.contrib.auth import login, authenticate
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from.forms import  CustomUserCreationForm  # You’ll create this form
 
-# Create your views here.
+# Registration view
+def register(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)  # Log in the user after successful registration
+            return redirect('home')  # Redirect to home page
+    else:
+        form = CustomUserCreationForm()
+    return render(request, 'register.html', {'form': form})
+
+# Login view (use Django’s built-in AuthenticationForm)
+def user_login(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('home')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'login.html', {'form': form})
