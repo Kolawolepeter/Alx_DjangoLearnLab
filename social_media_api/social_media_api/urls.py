93d6478985b298021e88bd5dc6e35a8d371d54b9
Kolawole Peter <oluwatayokolawole12@gmail.com>
2024-09-22 20:31:28 +0100

    GG

--- social_media_api/social_media_api/urls.py
+++ social_media_api/social_media_api/urls.py
@@ -3,3 +3,9 @@ from django.urls import path, include
 urlpatterns = [
     path('api/accounts/', include('accounts.urls')),
 ]
+
+from django.urls import path, include
+
+urlpatterns = [
+    path('api/', include('posts.urls')),
+]
