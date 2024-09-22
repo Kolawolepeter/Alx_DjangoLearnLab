292963a13e2029f928115cb8d6713d0f8820bb1f
Kolawole Peter <oluwatayokolawole12@gmail.com>
2024-09-22 19:54:30 +0100

    F

---
+++ social_media_api/social_media_api/accounts/views.py
@@ -0,0 +1,26 @@
+from rest_framework.authtoken.models import Token
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from .models import CustomUser
+from .serializers import UserSerializer
+from django.contrib.auth import authenticate
+
+@api_view(['POST'])
+def register(request):
+    if request.method == 'POST':
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            user = CustomUser.objects.create_user(**serializer.validated_data)
+            token, created = Token.objects.get_or_create(user=user)
+            return Response({'token': token.key})
+        return Response(serializer.errors)
+
+@api_view(['POST'])
+def login(request):
+    username = request.data.get('username')
+    password = request.data.get('password')
+    user = authenticate(username=username, password=password)
+    if user:
+        token, created = Token.objects.get_or_create(user=user)
+        return Response({'token': token.key})
+    return Response({'error': 'Invalid credentials'}, status=400)
